25fa1d7cbb8f05c08efcd307ae503970ff064764
Rasmus Wiuff <rwiuff@gmail.com>
2023-01-05 21:30:01 +0100

    New project

---
+++ BasicReversi/app/src/main/java/basicreversi/BoardDriver.java
@@ -0,0 +1,28 @@
+package basicreversi;
+
+import java.text.DecimalFormat;
+
+public class BoardDriver {
+    private static DecimalFormat df = new DecimalFormat(" 0");
+
+    public static void main(String[] args) {
+        Board b = new Board();
+        printBoard(b.getBoard());
+        b.flip(4, 4, 'w');
+        printBoard(b.getBoard());
+        System.out.println("Player 1: " + b.getPlayers().get("Player 1"));
+    }
+
+    private static void printBoard(int[][] b) {
+        int size = b.length;
+        System.out.println("#" + "---".repeat(size) + "#");
+        for (int row = 0; row < size; row++) {
+            System.out.print("|");
+            for (int col = 0; col < size; col++) {
+                System.out.print(df.format(b[row][col]) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("#" + "---".repeat(size) + "#");
+    }
+}
