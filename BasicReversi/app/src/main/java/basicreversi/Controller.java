523211d11a555cf58d3d83b57bd286c343aef6f8
AbinavReddy <122014198+AbinavReddy@users.noreply.github.com>
2023-01-05 13:43:31 +0100

    Restart

--- BasicReversi/app/src/main/java/basicreversi/Controller.java
+++ BasicReversi/app/src/main/java/basicreversi/Controller.java
@@ -1,11 +1,25 @@
-package application;
+package basicreversi;
+
+import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+	
+	
+
 
 public class Controller {
 	
@@ -13,6 +27,11 @@ public class Controller {
 
 	public TextArea textArea;
 	Circle myCircle = new Circle();
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	
 
 	public void confirmation(MouseEvent e) {
 		textArea.appendText("Circle clicked\n");
@@ -20,8 +39,21 @@ public class Controller {
 		
 	}
 	
-	public void printOutRestart(ActionEvent e) {
-		textArea.appendText("Game has been restarted\n");
+	
+	
+	public void Restart(ActionEvent e) throws IOException {
+		
+		root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+       scene = new Scene(root);
+       stage.setScene(scene);
+       stage.show();
+		
 		
 	}
+	
+	
+		
+		
+	
 }
---
+++ BasicReversi/app/src/main/java/basicreversi/ReversiView.java
@@ -0,0 +1,38 @@
+package Reversi;
+
+	import javafx.application.Application;
+	import javafx.event.*;
+	import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+	import javafx.stage.Stage;
+
+	public  class ReversiView  extends Application {
+
+	    public static void main(String[] args) {
+	    	launch(args);
+	    }
+
+		@Override
+		public void start(Stage stage) throws Exception {
+			
+			stage.setTitle("app");
+			
+			//if fxml does not work refersh project first, then try code below
+			//System.out.println(ClassLoader.getSystemResource("Reversi/f.fxml"));
+			Parent root = FXMLLoader.load(ClassLoader.getSystemResource("Reversi/f.fxml"));
+			
+			stage.setScene(new Scene(root,500,300));
+			
+			stage.show();
+		
+			
+			
+		}
+
+	}
+
+
+
+
+
