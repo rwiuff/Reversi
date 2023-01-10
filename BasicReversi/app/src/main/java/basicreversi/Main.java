a1c4c7cb7481831d7e22ed7e16226ead0603cab6
Rasmus Wiuff <rwiuff@gmail.com>
2023-01-10 13:39:02 +0100

    icons and shit

--- BasicReversi/app/src/main/java/basicreversi/Main.java
+++ BasicReversi/app/src/main/java/basicreversi/Main.java
@@ -1,8 +1,12 @@
 package basicreversi;
+
+import org.checkerframework.common.reflection.qual.GetClass;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +16,11 @@ public class Main extends Application {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
             Parent root = loader.load();
             Scene scene = new Scene(root);
+            primaryStage.getIcons().addAll(
+				new Image(getClass().getResourceAsStream("icon16.png")),
+				new Image(getClass().getResourceAsStream("icon32.png")),
+				new Image(getClass().getResourceAsStream("icon64.png"))
+			);
             Controller controller = loader.getController();
             controller.in();
             primaryStage.setTitle("Reversi");
