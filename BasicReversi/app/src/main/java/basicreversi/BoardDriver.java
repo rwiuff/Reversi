08acc4a7975dfa466bb25e00953bf275cac008d4
Rasmus Wiuff <rwiuff@gmail.com>
2023-01-10 22:08:13 +0100

    added tests to BoardDriver; panache to circles

--- BasicReversi/app/src/main/java/basicreversi/BoardDriver.java
+++ BasicReversi/app/src/main/java/basicreversi/BoardDriver.java
@@ -7,29 +7,61 @@ public class BoardDriver {
     private static DecimalFormat df = new DecimalFormat(" 0");
 
     public static void main(String[] args) {
+
+        // --------- Multi-directional test --------- //
+        System.out.println("--------- Multi-directional test ---------");
         Board a = new Board();
-        System.out.println(a. getGreeting());
+        System.out.println(a.getGreeting());
         a.setPiece(2, 3, 2);
         a.setPiece(2, 4, 1);
         a.setPiece(3, 2, 2);
-        a.setPiece(3,3,2);
+        a.setPiece(3, 3, 2);
         a.setPiece(3, 4, 1);
         a.setPiece(4, 2, 1);
-        a.setPiece(4,3,1);
+        a.setPiece(4, 3, 1);
         a.setPiece(4, 4, 2);
         printBoard(a.getBoard());
         a.turnState(1);
         a.place(2, 2, 1);
         printBoard(a.getBoard());
+        System.out.println("--------- Multi-directional test ---------");
+        System.out.println("");
+        // --------- Multi-directional test --------- //
+
+        // ------------ turnState test ------------- //
+        System.out.println("------------ turnState test -------------");
+        a.setPiece(4, 4, 1);
+        a.setPiece(3, 3, 1);
+        printBoard(a.getBoard());
+        System.out.println(a.turnState(1));
+        System.out.println("------------ turnState test -------------");
+        System.out.println("");
+        // ------------ turnState test ------------- //
+
+        // --------- Multi-directional test --------- //
+        System.out.println("--------- Multi-directional test ---------");
+        System.out.println(a.gameState());
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                a.setPiece(i, j, 1);
+            }
+        }
+        printBoard(a.getBoard());
+        System.out.println(a.gameState());
+        System.out.println(a.checkWinner());
+        System.out.println("--------- Multi-directional test ---------");
+        System.out.println("");
+        // --------- Multi-directional test --------- //
+
 
         Board b = new Board();
         Scanner console = new Scanner(System.in);
-        System.out.println(b.getPlayers().get("White") + ": White");
+        System.out.println("Hvid: " + b.getPlayers().get("White"));
         int colour;
         int opponent;
         String playastring;
         printBoard(b.getBoard());
-        while (b.getTurn()  < 2) {
+        while (b.getTurn() < 2) {
             System.out.println(b.getPlayers().get("White") + ", placer dine startbrikker: x,y");
             String moveStr;
             moveStr = console.next();
@@ -40,7 +72,7 @@ public class BoardDriver {
             printBoard(b.getBoard());
         }
         while (b.gameState() == 32) {
-            if(b.getTurn() % 2 == 0){
+            if (b.getTurn() % 2 == 0) {
                 colour = 2;
                 opponent = (colour == 2) ? 1 : 2;
                 playastring = b.getPlayers().get("Black");
@@ -49,16 +81,20 @@ public class BoardDriver {
                 opponent = (colour == 1) ? 2 : 1;
                 playastring = b.getPlayers().get("White");
             }
-            System.out.println(b.turnState(colour));
-            System.out.println(colour);
-            System.out.println(playastring + ", ya move: x,y");
-            String moveStr;
-            moveStr = console.next();
-            int[] move = new int[2];
-            move[0] = Integer.parseInt(moveStr.split(",")[0]);
-            move[1] = Integer.parseInt(moveStr.split(",")[1]);
-            System.out.println(b.place(move[0], move[1], colour));
-            printBoard(b.getBoard());
+            switch (b.turnState(colour)) {
+                case 21:
+                    System.out.println(playastring + " (" + colour + ") " + ", ya move: x,y");
+                    String moveStr;
+                    moveStr = console.next();
+                    int[] move = new int[2];
+                    move[0] = Integer.parseInt(moveStr.split(",")[0]);
+                    move[1] = Integer.parseInt(moveStr.split(",")[1]);
+                    System.out.println(b.place(move[0], move[1], colour));
+                    printBoard(b.getBoard());
+                    break;
+                case 22:
+                    System.out.println("No legal moves. Fofeit turn.");
+            }
         }
         System.out.println(b.checkWinner());
     }
