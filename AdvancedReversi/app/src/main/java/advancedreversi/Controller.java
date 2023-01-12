5450d224ae9ae80ddddd30c00b7815e4cc13c8be
Rasmus Wiuff <rwiuff@gmail.com>
2023-01-12 17:14:19 +0100

    Visual view works

--- AdvancedReversi/app/src/main/java/advancedreversi/Controller.java
+++ AdvancedReversi/app/src/main/java/advancedreversi/Controller.java
@@ -1,6 +1,8 @@
 package advancedreversi;
 
 import java.io.IOException;
+import java.util.Set;
+
 import javafx.animation.KeyFrame;
 import javafx.util.Duration;
 import javafx.animation.Timeline;
@@ -59,14 +61,15 @@ public class Controller {
 		int row = getRowIndex(event);
 		int column = getColumnIndex(event);
 		Pane pane = (Pane) event.getSource();
-		int color = startID;
+		int color;
 		String ownString;
 		String opponentString;
 		if (gameStarted) {
 			System.out.println("(" + row + "," + column + ")");
 			if (b.getTurn() < 2) {
-				firstFour(row, column, color, pane);
+				firstFour(row, column, pane);
 			} else {
+				hideLegalMoves();
 				color = (b.getTurn() % 2 == 1) ? startID : secondViolin;
 				ownString = (color == 1) ? b.getPlayers().get(1) : b.getPlayers().get(2);
 				opponentString = (color == 1) ? b.getPlayers().get(2) : b.getPlayers().get(1);
@@ -75,17 +78,19 @@ public class Controller {
 						label.setText("No legal moves. " + opponentString + "'s turn");
 						break;
 					case 21:
-						label.setText(ownString + "s turn");
 						switch (b.place(row, column, color)) {
 							case 11:
 								update();
 								label.setText("Good job!\n" + opponentString + "'s turn");
+								showLegalMoves((color == 1) ? 2 : 1);
 								break;
 							case 12:
 								label.setText("Field already occupied\n" + ownString + "'s turn");
+								showLegalMoves(color);
 								break;
 							case 13:
 								label.setText("Illegal move\n" + ownString + "'s turn");
+								showLegalMoves(color);
 								break;
 						}
 				}
@@ -143,6 +148,16 @@ public class Controller {
 			fill = Color.rgb(0, 0, 0);
 			c = new Circle(23, fill);
 			c.setStroke(stroke);
+		} else if (color == 3) {
+			stroke = Color.rgb(153, 153, 153);
+			fill = Color.rgb(204, 204, 204, 0.5);
+			c = new Circle(23, fill);
+			c.setStroke(stroke);
+		} else if (color == 4) {
+			stroke = Color.rgb(179, 179, 179);
+			fill = Color.rgb(0, 0, 0, 0.5);
+			c = new Circle(23, fill);
+			c.setStroke(stroke);
 		}
 		c.setStrokeWidth(3);
 		c.setCenterX(pane.getWidth() / 2);
@@ -150,13 +165,14 @@ public class Controller {
 		pane.getChildren().add(c);
 	}
 
-	public void firstFour(int row, int column, int startID, Pane pane) {
+	public void firstFour(int row, int column, Pane pane) {
 		String playerTurn = (startID == 1) ? b.getPlayers().get(1) : b.getPlayers().get(2);
 		switch (b.initPlace(row, column, startID)) {
 			case 11:
 				update();
 				if (b.getTurn() == 1)
 					label.setText(playerTurn + ": Place second tile");
+				showLegalMoves(startID);
 				if (b.getTurn() > 1)
 					label.setText(playerTurn + ": Your move!");
 				break;
@@ -177,6 +193,28 @@ public class Controller {
 				Pane pane = (Pane) gridPane.lookup(paneID);
 				pane.getChildren().clear();
 			}
+		}
+	}
+
+	public void showLegalMoves(int colour) {
+		b.moveAnalyser(colour);
+		int circleColour = (colour == 1) ? 3 : 4;
+		Set<String> validMovesSet = b.getValidMoves().keySet();
+		String[] validMoves = validMovesSet.toArray(new String[validMovesSet.size()]);
+		for (int i = 0; i < validMoves.length; i++) {
+			String paneID = "#" + validMoves[i].replace(",", "");
+			Pane pane = (Pane) gridPane.lookup(paneID);
+			drawCircle(circleColour, pane);
+		}
+	}
+
+	public void hideLegalMoves() {
+		Set<String> validMovesSet = b.getValidMoves().keySet();
+		String[] validMoves = validMovesSet.toArray(new String[validMovesSet.size()]);
+		for (int i = 0; i < validMoves.length; i++) {
+			String paneID = "#" + validMoves[i].replace(",", "");
+			Pane pane = (Pane) gridPane.lookup(paneID);
+			pane.getChildren().clear();
 		}
 	}
 }
